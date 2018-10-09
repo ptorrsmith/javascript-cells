164fe4a6a8824b7f4df14f8653cfe1f94898fef1
ptorrsmith <peter@ptorrsmith.com>
2018-10-09 17:18:31 +1300

    Empty Multiverse HTML. Create all by JS

--- scripts/jscells.js
+++ scripts/jscells.js
@@ -12,26 +12,52 @@ document.addEventListener('DOMContentLoaded', startCells);
 // Define your `multiverse` object here!
 var multiverse = {
     universes: [
-    ]  
+    ]
 }
-
-var universes = {
-    cells: []
-}
-
 
 
 function startCells() {
-    // load multiverse
+    loadMultiverse(1);
+    loadUniverses(2);
 
 
 }
 
 
 
-function loadMultiverse() {
-    // create a two cells (divs of class="cell grid")
-    // 
+function loadMultiverse(count) {
+    // clear out existing html
+    let multiverse = document.getElementsByClassName('multiverse')[0];
+    multiverse.innerHTML = "<span>The multiverse</span>";
+    // create a single universe and put into the multiverse
+    let universe = document.createElement('div');
+    universe.className = "universe cell grid";
+    universe.id = "cell-0";
+    let uvTitle = document.createElement('span');
+    uvTitle.innerHTML = "The Universe";
+    universe.appendChild(uvTitle);
+    // $('body').append(universe);
+    multiverse.appendChild(universe);
 
 }
-  
+
+function loadUniverses(count) {
+    // loop through all universes and add count child cells
+    // only 1 universe for inital app
+    let universes = document.getElementsByClassName('universe');
+    for (i = 0; i < universes.length; i++) {
+        // do count times
+        for (j = 0; j < count; j++) {
+            let cell = document.createElement('div');
+            cell.className = "child cell grid";
+            cell.id = "cell-" + (i+1) + "." + (j+1);
+
+            let cellTitle = document.createElement('span');
+            cellTitle.innerHTML = "child cell " + (i+1) + "." + (j+1);
+            cell.appendChild(cellTitle);
+            universes[i].appendChild(cell);
+
+        }
+    }
+
+}
