786f1d98d4612658fe5aaff8f52d0fa541b9b2a1
ptorrsmith <peter@ptorrsmith.com>
2018-10-11 14:28:21 +1300

    Work through recursive reductions - not working right but a step forward

--- scripts/jscells.js
+++ scripts/jscells.js
@@ -8,37 +8,23 @@ It is used to construct a multiverse object with cell objects which have propert
 let multiverse; /// so this is in our global scope
 // note: multiverse is just a cell, with a name, description, ancestry (of 0), and optional markdown
 
+
 // [ts] This constructor function may be converted to a class declaration.
 // function JSCell(name: any, description: any, ancestry: any, markdown: any): void
-// ?? Constructor functions vs class declarations?
-// ?? cells is an array, or should we make it a cells collection object, with a name and a cells array?
-function JSCell(name, description, ancestry, markdown) {
-    this.name = name;
+// ?? Constructor functions vs class declarations?  
+// and what is super().  extends?  (OOP?)
+// ?? cells is an array, or should we make it a cells collection object, with a name and a cells array?  Future.  Array for now to keep simple.
+
+
+function JSCell(name = 'A Cell', description = '', classProp = '', ancestry = '', markdown = '') {
+    this.name = name; // this means the object created with new JSCell()
     this.description = description;
+    this.cellClass = `cell grid ${classProp.length > 0 ? ' ' + classProp : ''}`; 
     this.ancestry = ancestry;
     this.markdown = markdown;
     this.cells = [];
     // future: method to update ancestry of all children
     // future: this.cellsCollection = new JSCellsCollection
+
 }
 
-
-
-
-
-
-
-
-
-
-// function Multiverse(name = "This Crazy Hihi Universe 2018!", cells = 2) { // can in future take a cells collection to load from other instance, export, save etc (JSON).  All cells should have this ability (for moving branches) 
-//     this.name = name;
-//     this.cells = []; // will need to populate this with cells x child cells (e.g. 2 child cells)
-//     // this.cellsCollection = {
-//     //     name: "Multiverse Cells",
-//     //     cells: [new JSCell("This Crazy Hihi Universe 2018!", "Our current reality ;-)", "")]
-//     for (i = 0; i < cells; i++) {
-//         let cell = new JSCell('Child Cell ${i}', 'A cell with no children','1.1.2.1.7.4.8.1', '# Cell #${i}');
-//         this.cells.push(cell);
-//     }
-// }
